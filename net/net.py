003230c407bacdaba7d31b3655b0797d06662683
Jon Martini <39231017+jsmartini@users.noreply.github.com>
2021-02-24 11:40:36 -0600

    code skeleton

---
+++ net/net.py
@@ -0,0 +1,21 @@
+import socketserver
+import asyncio
+from queue import LifoQueue as STACK
+
+
+class ConnectionDaemon(object):
+
+    def __init__(self):
+        pass
+
+    def send(self, data):
+        pass
+
+    def recv(self):
+        pass
+
+    async def _send(self):
+        pass
+
+    async def _recv(self):
+        pass
