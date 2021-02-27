996059978162310843036fa4145ee422efdcbcca
Jon Martini <39231017+jsmartini@users.noreply.github.com>
2021-02-27 00:46:59 -0600

    project restructure

--- net_test_rpi.py
+++ net_test_rpi.py
@@ -2,13 +2,12 @@ from p2pnet import Net
 import logging
 import asyncio
 if __name__ == "__main__":
-
+    logging.basicConfig(filename="net_test_rpi.log", level=logging.INFO)
     net = Net(
         "",
         4444,
         node_type=False
     )
-    logging.basicConfig(filename="net_test_rpi.log", level=logging.INFO)
     async def main():
         while 1:
             net.to_send(net.recv())
